c44cff89f8e7622584a56d9edc16cb2d72f2c50b
kodowicz <annakodowicz@gmail.com>
2021-02-09 12:28:08 +0100

    create theme switcher

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
 import { lightTheme, darkTheme, GlobalStyle } from "../assets/styles";
 
 const Theme = ({ children }) => {
+  const { isLight } = useSelector(state => ({
+    isLight: state.theme.isLight
+  }));
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
       <>
         <GlobalStyle />
         { children }
